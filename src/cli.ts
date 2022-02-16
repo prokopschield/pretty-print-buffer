462b005c253270f75a80c32e44d09276906182d2
Prokop <prokop.schield@protonmail.com>
2022-02-16 17:11:35 +0100

    v0.1.0-3: allow passing files as arguments

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,49 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
 import pretty from '.';
 
-process.stdin.on('data', (b) => process.stdout.write(pretty(b)));
+let flags: {
+	[flag: string]: boolean;
+} = {};
+
+async function processFile(file: string) {
+	flags['no-implicit-stdin'] = true;
+	const stat = await fs.promises.stat(file);
+	if (!stat.isFile()) {
+		throw new Error(`${file} is not a file.`);
+	}
+	const stream = fs.createReadStream(file);
+	stream.on('data', (chunk) =>
+		process.stdout.write(pretty(Buffer.from(chunk)))
+	);
+	return new Promise((resolve) => stream.on('end', resolve));
+}
+
+enum READMODE {
+	DEFAULT,
+}
+
+async function main() {
+	let readmode: READMODE = READMODE.DEFAULT;
+	for (const arg of process.argv.slice(2)) {
+		try {
+			switch (readmode) {
+				default: {
+					switch (arg[0]) {
+						default: {
+							await processFile(arg);
+						}
+					}
+				}
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}
+	if (flags['read-stdin'] || !flags['no-implicit-stdin']) {
+		process.stdin.on('data', (b) => process.stdout.write(pretty(b)));
+	}
+}
+
+main();
